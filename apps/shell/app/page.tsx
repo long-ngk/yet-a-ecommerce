0945269009873e2bc54bc815fa140f438d69a0e2
Long Nguyen <75737464+long-ngk@users.noreply.github.com>
2026-07-05 19:58:34 +0700

    refactor: remove list on home screen

--- apps/shell/app/page.tsx
+++ apps/shell/app/page.tsx
@@ -1,25 +1,7 @@
-import Link from "next/link";
-
 export default function HomePage() {
   return (
     <main>
       <h1>Welcome to E-Commerce Platform</h1>
-      <nav>
-        <ul>
-          <li>
-            <Link href="/products">Products</Link>
-          </li>
-          <li>
-            <Link href="/orders">Orders</Link>
-          </li>
-          <li>
-            <Link href="/account">Account</Link>
-          </li>
-          <li>
-            <Link href="/checkout">Checkout</Link>
-          </li>
-        </ul>
-      </nav>
     </main>
   );
 }
